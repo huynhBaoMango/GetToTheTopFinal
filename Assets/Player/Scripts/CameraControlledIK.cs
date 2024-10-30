8d2861d54578defda3b016867af34edd86cda342
Freakerq <150575354+Freakerq@users.noreply.github.com>
2024-10-30 12:16:16 +0700

    Up player

---
+++ Assets/Player/Scripts/CameraControlledIK.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace scgFullBodyController
+{
+    public class CameraControlledIK : MonoBehaviour
+    {
+        public Transform spineToOrientate;
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            spineToOrientate.rotation = transform.rotation;
+        }
+    }
+}
---
+++ Assets/Player/Scripts/sceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace scgFullBodyController
+{
+    public class sceneController : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
