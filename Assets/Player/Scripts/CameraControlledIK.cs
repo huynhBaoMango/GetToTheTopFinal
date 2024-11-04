a2b54d375b5b01dba746f66cdd6a556f5b2a0a4e
huynhBaoMango <140953773+huynhBaoMango@users.noreply.github.com>
2024-11-04 14:22:07 +0700

    fix

--- Assets/Player/Scripts/CameraControlledIK.cs
+++ Assets/Player/Scripts/CameraControlledIK.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using FishNet.Object;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace scgFullBodyController
 {
-    public class CameraControlledIK : MonoBehaviour
+    public class CameraControlledIK : NetworkBehaviour
     {
         public Transform spineToOrientate;
 
