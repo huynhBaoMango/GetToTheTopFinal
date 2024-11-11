15b8e713d6593053fd4af440e3fb3b9f46da845f
huynhBaoMango <140953773+huynhBaoMango@users.noreply.github.com>
2024-11-12 01:37:11 +0700

    zz

--- Assets/PlayerWeapon.cs
+++ Assets/PlayerWeapon.cs
@@ -1,16 +1,47 @@
+using FishNet.Object;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerWeapon : MonoBehaviour
+public class PlayerWeapon : NetworkBehaviour
 {
     [SerializeField] private List<APlayerWeapon> weapons = new List<APlayerWeapon>();
+    [SerializeField] private APlayerWeapon currentWeapon;
 
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (!base.IsOwner)
+        {
+            enabled = false;
+            return;
+        }
+    }
     public void InitializeWeapons(Transform parentOfWeapons)
     {
         for (int i = 0; i < weapons.Count; i++)
         {
             weapons[i].transform.parent = parentOfWeapons;
-        } 
+        }
+
+        InitializeWeapon(0);
+    }
+
+    private void InitializeWeapon(int weaponIndex)
+    {
+        for(int i = 0;i < weapons.Count;i++)
+        {
+            weapons[i].gameObject.SetActive(false);
+        }
+
+        if (weapons.Count > weaponIndex)
+        {
+            currentWeapon = weapons[weaponIndex]; 
+        }
+    }
+
+    private void FireWeapon()
+    {
+        currentWeapon.Fire();
     }
 }
