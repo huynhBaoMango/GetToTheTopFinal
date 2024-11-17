229a4ddc2d58a246ae7a0a78fe2e6059eae7bf43
ntdung261004 <ntdung261004@gmail.com>
2024-11-17 23:25:22 +0700

    lobby update 1

---
+++ Assets/Scripts/Lobby/GameplayManager.cs
@@ -0,0 +1,36 @@
+//using FishNet.Object;
+//using FishNet.Object.Synchronizing;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class GameplayManager : NetworkBehaviour
+//{
+//    public static GameplayManager instance2;
+//    public GameObject StartGameButton;
+
+//    public readonly SyncList<PlayerLobby> players = new SyncList<PlayerLobby>();
+
+//    private void Awake()
+//    {
+//        instance2 = this;
+//    }
+
+//    private void Update()
+//    {
+//        if (!IsServerInitialized)
+//            return;
+
+//        bool allPlayersReady = players.Count > 0;
+//        foreach (PlayerLobby player in players)
+//        {
+//            if (!player.IsReady)
+//                allPlayersReady = false;
+//        }
+
+//        if (allPlayersReady && !StartGameButton.activeSelf)
+//            StartGameButton.SetActive(true);
+//        else if (!allPlayersReady && StartGameButton.activeSelf)
+//            StartGameButton.SetActive(false);
+//    }
+//}
---
+++ Assets/Scripts/Lobby/PlayerLobby.cs
@@ -0,0 +1,119 @@
+﻿//using FishNet.Object;
+//using FishNet.Object.Synchronizing;
+//using NaughtyAttributes;
+//using System.Collections;
+//using System.Collections.Generic;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class PlayerLobby : NetworkBehaviour
+//{
+//    public static PlayerLobby instance3;
+
+//    [SerializeField]
+//    GameObject lobbyPlayerCard;
+
+//    [SerializeField]
+//    TextMeshProUGUI playerNameText;
+
+//    [SerializeField]
+//    TextMeshProUGUI playerReadyText;
+
+
+//    [SyncVar]
+//    public bool IsReady;
+
+//    [SyncVar]
+//    public string playerName;
+
+//    [field: SerializeField]
+//    [field: SyncVar(OnChange = nameof(OnChangePlayerReady))]
+//    public bool IsReady { get; private set; }
+
+//    [field: SerializeField]
+//    [field: SyncVar(OnChange = nameof(OnChangePlayerName))]
+//    public string playerName { get; private set; }
+
+//    private void OnChangePlayerReady(bool oldValue, bool newValue, bool isServer)
+//    {
+//        if (!isServer)
+//        {
+//            playerNameText.text = playerName;
+//            if (IsReady)
+//                playerReadyText.text = "READY";
+//            else playerReadyText.text = "NOT READY";
+//        }
+
+//    }
+
+//    private void OnChangePlayerName(bool oldValue, bool newValue, bool isServer)
+//    {
+//        if (!isServer)
+//        {
+//            playerNameText.text = playerName;
+//            if (IsReady)
+//                playerReadyText.text = "READY";
+//            else playerReadyText.text = "NOT READY";
+//        }
+//    }
+
+//    //Cap nhat khi playerReady thay doi
+//    private void UpdatePlayerReadyText()
+//    {
+//        if (IsReady)
+//            playerReadyText.text = "READY";
+//        else
+//            playerReadyText.text = "NOT READY";
+//    }
+
+//    // Cap nhat khi playerName thay doi
+//    private void UpdatePlayerNameText()
+//    {
+//        playerNameText.text = playerName;
+//    }
+
+//    public override void OnStartClient()
+//    {
+//        base.OnStartClient();
+
+//        GameObject playersCardPanel = GameObject.Find("Panel_Players");
+//        if (!playersCardPanel)
+//            Debug.LogError("UI lobby panel for players not found");
+//        else
+//        {
+//            GameObject playerCard = Instantiate(lobbyPlayerCard, playersCardPanel.transform);
+//            playerNameText = playerCard.GetComponentInChildren<TextMeshProUGUI>();
+//            playerReadyText = playerCard.transform.Find("txtPlayerReady")?.GetComponent<TextMeshProUGUI>();
+//            if (!playerNameText)
+//                Debug.LogError("playerNameText not found");
+//        }
+
+//        if (!IsOwner)
+//            return;
+
+//        instance3 = this;
+//        ChangePlayerName("Player: " + OwnerId);
+//    }
+
+//    public override void OnStartServer()
+//    {
+//        base.OnStartServer();
+//        GameplayManager.instance2.players.Add(this);
+//    }
+
+//    [ServerRpc]
+//    public void ToggleReadyState()
+//    {
+//        IsReady = !IsReady;
+//        UpdatePlayerReadyText(); // Cap nhat trang thai "READY" hay "NOT READY"
+//    }
+
+//    [ServerRpc]
+//    public void ChangePlayerName(string name)
+//    {
+//        name = Common.instance.currentUser.username;
+//        playerName = name;
+//        UpdatePlayerNameText(); // Cap nhat ten nguoi choi
+//    }
+//}
